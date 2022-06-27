5efa8e6bb3b5bc8b3d23c94534548360b4bf5ede
VandalSausage <trumanwright13@gmail.com>
2022-06-27 19:13:31 +0100

    account for weight in the calc cost

--- src/calc-output/calc-output.ts
+++ src/calc-output/calc-output.ts
@@ -1,6 +1,18 @@
 import { ParcelType, ParcelInput, Output, ParcelOutput } from "../types";
 
-const getCost = (maxDimension: number): { cost: number; type: ParcelType } => {
+const weightLookup: Record<ParcelType, number> = {
+  small: 1,
+  medium: 3,
+  large: 6,
+  xl: 10,
+};
+
+const getWeightCost = (weight: number, type: ParcelType) =>
+  Math.max(0, weight - weightLookup[type]) * 2;
+
+const getBaseCost = (
+  maxDimension: number
+): { cost: number; type: ParcelType } => {
   switch (true) {
     case maxDimension < 10:
       return { cost: 3, type: "small" };
@@ -12,12 +24,21 @@ const getCost = (maxDimension: number): { cost: number; type: ParcelType } => {
       return { cost: 25, type: "xl" };
   }
 };
+const getCost = (
+  maxDimension: number,
+  weight: number
+): { cost: number; type: ParcelType } => {
+  const { cost: baseCost, type } = getBaseCost(maxDimension);
+  const weightCost = getWeightCost(weight, type);
+  return { cost: baseCost + weightCost, type };
+};
+
 const getParcel = (
-  { height, width, depth }: ParcelInput,
+  { height, width, depth, weight }: ParcelInput,
   index: number
 ): ParcelOutput => ({
   name: `Parcel ${index + 1}`,
-  ...getCost(Math.max(height, width, depth)),
+  ...getCost(Math.max(height, width, depth), weight),
 });
 
 export const calculateOutput = (records: ParcelInput[]): Output =>
