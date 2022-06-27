b497e7afce17b134641b564fdaf582be5f8dd44e
VandalSausage <trumanwright13@gmail.com>
2022-06-27 19:00:53 +0100

    add ability to consume a config bundle

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
+import { readFileSync } from "fs";
 import { calculateOutput } from "./calc-output";
 import { parseCsv, writeOutput } from "./csv";
+import { Config } from "./types";
+
+const getConfig = (): Config => {
+  try {
+    const config = JSON.parse(
+      readFileSync("config.json", { encoding: "utf-8" })
+    );
+    return { isSpeedy: !!config.isSpeedy };
+  } catch (e) {
+    console.error(e);
+    console.log("could not parse params - choosing defaults");
+    return { isSpeedy: false };
+  }
+};
 
 const main = async () => {
+  const config = getConfig();
   const input = parseCsv();
   const output = calculateOutput(input);
-  await writeOutput(output);
+  await writeOutput(output, config);
 };
 
 main();
