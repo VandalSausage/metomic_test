09891341db7c6ed12a03b4fd5acc419d0eac4e41
VandalSausage <trumanwright13@gmail.com>
2022-06-27 18:36:09 +0100

    add boiler plate + ability to run index.ts using node

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "14",
+        },
+        modules: "auto",
+      },
+    ],
+    "@babel/preset-typescript",
+  ],
+};
---
+++ index.js
@@ -0,0 +1,3 @@
+require("@babel/register")({ extensions: [".ts"] });
+
+module.exports = require("./src/index");
